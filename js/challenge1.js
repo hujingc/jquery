f95ed0b3e28f889eb09c81ba10f0b8f0fe2eda4c
hujingc <42220872+hujingc@users.noreply.github.com>
2019-11-24 19:25:48 -0500

    prep, initial start assignment 1

---
+++ js/challenge1.js
@@ -0,0 +1,20 @@
+$(document).ready(function(event){ 
+    $('#emailField').click(function(event){
+        
+    })
+  });
+  subscribe.onchange = function() {
+	var x = document.querySelector("#emailField");
+	if (this.checked){
+		console.log("selected");
+    	x.style.display = "block";
+  	} else {
+  		console.log("Not selected");
+    	x.style.display = "none";
+  	}
+  // 	Advanced Option....
+  // 	x.style.display == "block" ? 
+		// x.style.display = "none" : 
+		// x.style.display = "block"; 
+}
+
