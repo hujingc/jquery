a4fd35f97f00f1dfd06205768e9b229732b2f9fc
hujingc <42220872+hujingc@users.noreply.github.com>
2019-11-26 04:03:04 -0500

    4's ok

--- js/challenge4.js
+++ js/challenge4.js
@@ -4,15 +4,15 @@ $('form').submit(function (event) {
     if ($("#fullname").val() == '') {
         console.log("there's no name")
         $("#nameerrormsg").show();
+        event.preventDefault();
     } else {
         $("#nameerrormsg").hide();
     }
 //if address is empty
     if ($("#streetaddr").val() == '') {
         $("#addrerrormsg").show();
+        event.preventDefault();
     } else {
         $("#addrerrormsg").hide();
     }
-    event.preventDefault();
 });
-console.log("the script's here")
