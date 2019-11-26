3205189561020cfc0e71a7f6c699436d45e5e59a
hujingc <42220872+hujingc@users.noreply.github.com>
2019-11-26 17:12:40 -0500

    c3 to chained ifelse

--- js/challenge3.js
+++ js/challenge3.js
@@ -1,17 +1,20 @@
 $('form').submit(function () {
-
-    if ($("input[name='fruit']").is(':checked')) {
-        console.log('a fruit is picked')
-    } else {
-        console.log('pick a fruit')
+    let fruit = $("input[name='fruit']").is(':checked');
+    let stand = $("input[name='standing']").is(':checked');
+    if (fruit & standing) {
+        console.log('everything is fine')
+    } else if (!fruit & standing) {
+        console.log('pick a fruit, we have standing')
         alert("You must pick a fruit!")
         return false
     }
-    if ($("input[name='standing']").is(':checked')) {
-        console.log('a standing is picked')
+    else if (fruit & !standing) {
+        alert("You must pick a class standing!")
+        console.log('pick a standing, we have fruit')
+        return false
     } else {
-        console.log('pick a standing')
-        alert("You must pick a standing!")
+        console.log('have not picked anything')
+        alert("You must pick a fruit and a class standing!")
         return false
     }
 })
