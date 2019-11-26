f94932c04054e9e0f1bdd16ed649438f42649e24
hujingc <42220872+hujingc@users.noreply.github.com>
2019-11-26 17:15:05 -0500

    c3 is good and cleaned

--- js/challenge3.js
+++ js/challenge3.js
@@ -3,15 +3,13 @@ $('form').submit(function () {
     let standing = $("input[name='standing']").is(':checked');
     if (fruit & standing) {
         console.log('everything is fine')
-
     } else if (!fruit & standing) {
         console.log('pick a fruit, we have standing')
         alert("You must pick a fruit!")
         return false
-    }
-    else if (fruit & !standing) {
-        alert("You must pick a class standing!")
+    } else if (fruit & !standing) {
         console.log('pick a standing, we have fruit')
+        alert("You must pick a class standing!")
         return false
     } else {
         console.log('have not picked anything')
