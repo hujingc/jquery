224e97bb92e5a7c7f63dfc6108d6c8fd4ecd9deb
hujingc <42220872+hujingc@users.noreply.github.com>
2019-11-26 20:19:44 -0500

    cleaned up a bit, still tab+mouse idk

--- js/challenge5.js
+++ js/challenge5.js
@@ -1,53 +1,20 @@
 $(document).ready(function () {
-    $('.preview').focus(function(){
-        $('#image').css("background-image", 'url(' + $(this).attr('src')+')')
+    $('.preview').focus(function () {
+        $('#image').css("background-image", 'url(' + $(this).attr('src') + ')')
         $('#image').html($(this).attr('alt'))
     })
-    $('.preview').hover(function(){
-        $('#image').css("background-image", 'url(' + $(this).attr('src')+')')
-        $('#image').html($(this).attr('alt'))}
-        ,function(){
-        $('#image').css("background-image",'')
+
+    $('.preview').blur(function () {
+        $('#image').css("background-image", '')
         $('#image').html("Hover over an image below.")
     })
-    $('.preview').blur(function(){
-        $('#image').css("background-image",'')
+
+    $('.preview').hover(function () {
+        $('#image').css("background-image", 'url(' + $(this).attr('src') + ')')
+        $('#image').html($(this).attr('alt'))
+    }, function () {
+        $('#image').css("background-image", '')
         $('#image').html("Hover over an image below.")
     })
-}
-    // $('#image').css("background-image", 'url(' + $(this).attr('src') + ')')
-)
-// $(function() {
-//     $("img").mouseover(function() {
-//         $("#image").css("background-image", "url('"+$(this).attr("src")+"')");
-//         $('#image').html($(this).attr("alt"));
-//     })
-//     $("img").mouseout(function() {
-//         $("#image").css("background-image", "");
-//         $('#image').html("Hover over an image below.");
-//     });
-// // });
-// console.log("here")
-// var imgs = document.querySelectorAll("img");
-// var msg = "Hover over an image below."
-// for (let i = 0; i < imgs.length; i++) {
-//     imgs[i].onmouseover = function () {
-//         document.querySelector('#image').style.backgroundImage = "url('" + imgs[i].src + "')";
-//         document.querySelector('#image').innerHTML = imgs[i].alt;
-//     }
-
-//     imgs[i].onfocus = function () {
-//         document.querySelector('#image').style.backgroundImage = "url('" + imgs[i].src + "')";
-//         document.querySelector('#image').innerHTML = imgs[i].alt;
-//     }
-
-//     imgs[i].onmouseleave = function () {
-//         document.querySelector('#image').style.backgroundImage = "url('')";
-//         document.querySelector('#image').innerHTML = msg;
-//     }
-
-//     imgs[i].onblur = function () {
-//         document.querySelector('#image').style.backgroundImage = "url('')";
-//         document.querySelector('#image').innerHTML = msg;
-//     }
-// }
+    
+})
